faf4850c4b92a3a8d58313b4b65a64a67c72148b
Fabio Berta <fnberta@gmail.com>
2023-08-07 00:27:05 +0200

    default to phone in preview

--- app/src/main/java/ch/berta/fabio/tipee/tip/TipScreen.kt
+++ app/src/main/java/ch/berta/fabio/tipee/tip/TipScreen.kt
@@ -34,6 +34,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ch.berta.fabio.tipee.R
@@ -180,7 +181,7 @@ private fun TipResult(heading: String, value: String, modifier: Modifier = Modif
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, device = Devices.PHONE)
 @Composable
 private fun TipScreenPreview() {
     TipeeTheme(dynamicColor = false) { TipScreen() }
